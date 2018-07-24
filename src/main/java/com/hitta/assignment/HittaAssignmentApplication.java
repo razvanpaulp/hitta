86a13afae95cbaee86f34804494813b98e6119af
Razvan Prichici <razvan.prichici@partner.bmw.de>
2018-07-25 01:35:35 +0300

    Initial commit

---
+++ src/main/java/com/hitta/assignment/HittaAssignmentApplication.java
@@ -0,0 +1,12 @@
+package com.hitta.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HittaAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HittaAssignmentApplication.class, args);
+	}
+}
